5c844618f8d44696930ca01ac1db837666c1d0f2
mrwindupbirdesq <nmaahs11@gmail.com>
2024-07-21 17:15:46 -0700

    Import navbar, render multiple cards for dev purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
 import { bonsaiCardData } from './bonsaiProfDummyData';
 import BonsaiCard from './components/bonsaiCard/BonsaiCard';
+import NavBar from './components/navBar/NavBar';
 
 function App() {
   return (
     <div className="container">
-      <h1>
-        Welcome to Bonsai Book. Sorry it's a bit spare right now, we're working
-        to amend that :)
-      </h1>
+      <NavBar />
+      <hr />
+      <BonsaiCard cardData={bonsaiCardData} />
+      <BonsaiCard cardData={bonsaiCardData} />
       <BonsaiCard cardData={bonsaiCardData} />
     </div>
   );
