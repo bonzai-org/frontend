34fdb78cc3373b57f9e349b508dc6224f6ac7994
mrwindupbirdesq <nmaahs11@gmail.com>
2024-07-24 21:49:29 -0700

    Add mock auth to demo auth forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import { bonsaiCardData } from './bonsaiProfDummyData';
 import BonsaiCard from './components/bonsaiCard/BonsaiCard';
+import AuthForm from './components/authForm/AuthForm';
 import NavBar from './components/navBar/NavBar';
 
 function App() {
-  return (
-    <div className="container">
-      <NavBar />
-      <hr />
-      <BonsaiCard cardData={bonsaiCardData} />
-      <BonsaiCard cardData={bonsaiCardData} />
-      <BonsaiCard cardData={bonsaiCardData} />
-    </div>
-  );
-}
+  const [auth, setAuth] = useState(true);
+  const toggleAuth = () => {
+    setAuth(!auth);
+  };
 
+  if (!auth) {
+    return (
+      <div className="container">
+        <AuthForm />
+        <button onClick={toggleAuth}>Show me the home page</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <NavBar />
+        <hr />
+        <button onClick={toggleAuth}>Show me the login</button>
+        <BonsaiCard cardData={bonsaiCardData} />
+        <BonsaiCard cardData={bonsaiCardData} />
+        <BonsaiCard cardData={bonsaiCardData} />
+      </div>
+    );
+  }
+}
 export default App;
