a8bbf84895bfabb386e385fd8b2a8f34adb48b1b
mrwindupbirdesq <nmaahs11@gmail.com>
2024-07-28 17:53:03 -0700

    Import new components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import './App.css';
 import { bonsaiCardData } from './bonsaiProfDummyData';
 import BonsaiCard from './components/bonsaiCard/BonsaiCard';
+import BonsaiDataForm from './components/bonsaiDataForm/BonsaiDataForm';
+import BonsaiChapterForm from './components/bonsaiChapterForm/BonsaiChapterForm';
+import BonsaiSubmitForm from './components/bonsaiSubmitForm/BonsaiSubmitForm';
+import BonsaiUpload from './components/bonsaiUpload/BonsaiUpload';
 import AuthForm from './components/authForm/AuthForm';
 import NavBar from './components/navBar/NavBar';
 
@@ -24,9 +28,13 @@ function App() {
         <NavBar />
         <hr />
         <button onClick={toggleAuth}>Show me the login</button>
+      {/*   <BonsaiCard cardData={bonsaiCardData} />
         <BonsaiCard cardData={bonsaiCardData} />
-        <BonsaiCard cardData={bonsaiCardData} />
-        <BonsaiCard cardData={bonsaiCardData} />
+        <BonsaiCard cardData={bonsaiCardData} /> */}
+{/*         <BonsaiDataForm />  */}
+{/*         <BonsaiChapterForm /> */}
+{/* <BonsaiSubmitForm bonsaiData={{ hardiness_zone: '7', height: 12, width: 10, nebari: 5, style: 'informal upright', species: 'Japanese Maple' }} bonsaiChapterArr={[]} /> */}
+<BonsaiUpload />
       </div>
     );
   }
