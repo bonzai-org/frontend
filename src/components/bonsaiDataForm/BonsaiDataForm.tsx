1160248bb36021cacb01462177a8e310e206d252
mrwindupbirdesq <nmaahs11@gmail.com>
2024-08-12 17:09:47 -0700

    Enforce hardiness zone validity Hardiness zones range from 0a to 13b. Update interface and jsx to reflect that.

--- src/components/bonsaiDataForm/BonsaiDataForm.tsx
+++ src/components/bonsaiDataForm/BonsaiDataForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import style from './BonsaiDataForm.module.css';
 
 interface BonsaiData {
-  hardiness_zone: string;
+  hardiness_zone: string; // Ensure this is a string
   height: number | '';
   width: number | '';
   nebari: number | '';
@@ -41,17 +41,30 @@ const BonsaiDataForm: React.FC<BonsaiDataFormProps> = ({
     onSubmit(bonsaiData);
   };
 
+  const hardinessZones = [];
+  for (let i = 0; i <= 13; i++) {
+    hardinessZones.push(`${i}a`, `${i}b`);
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Add Bonsai Data</h2>
       <div>
         <label>Hardiness Zone:</label>
-        <input
-          type="text"
+        <select
           value={hardinessZone}
           onChange={(e) => setHardinessZone(e.target.value)}
           required
-        />
+        >
+          <option value="" disabled>
+            Select Hardiness Zone
+          </option>
+          {hardinessZones.map((zone) => (
+            <option key={zone} value={zone}>
+              {zone}
+            </option>
+          ))}
+        </select>
       </div>
       <div>
         <label>Height:</label>
