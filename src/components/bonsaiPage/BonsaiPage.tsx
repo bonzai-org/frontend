ce81b26e3f35ab750079f878faaa2049f3167b2e
mrwindupbirdesq <nmaahs11@gmail.com>
2024-08-24 17:36:33 -0700

    Create bonsai page component

---
+++ src/components/bonsaiPage/BonsaiPage.tsx
@@ -0,0 +1,20 @@
+import styles from './BonsaiPage.module.css';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function BonsaiPage() {
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    console.log('Fetching bonsai with ID: ', id);
+  }, [id]);
+
+  return (
+    <div>
+      <h1>Bonsai Page</h1>
+      <p>Displaying details for bonsai with ID: {id}</p>
+    </div>
+  );
+}
+
+BonsaiPage;
