05dfa84debf626c67f5f8032d4668a604f85eb76
mrwindupbirdesq <nmaahs11@gmail.com>
2024-08-26 17:40:18 -0700

    Update interface, rename for clarity

--- src/components/bonsaiPage/BonsaiPage.tsx
+++ src/components/bonsaiPage/BonsaiPage.tsx
@@ -1,18 +1,46 @@
-import styles from './BonsaiPage.module.css';
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { bonsaiData } from '../../bonsaiProfDummyData';
+
+interface BonsaiChapter {
+  photoUrls: string[];
+  date: Date;
+  caption: string;
+  bonsaiId: string;
+}
+
+interface Bonsai {
+  id: string;
+  user: string;
+  species: string;
+  geoLocation: string;
+  style: string;
+  height: string;
+  width: string;
+  nebari: string;
+  hardinessZone: string;
+  bonsaiChapters: BonsaiChapter[];
+}
 
 export default function BonsaiPage() {
   const { id } = useParams<{ id: string }>();
+  const [bonsai, setBonsai] = useState<Bonsai | null>(null);
 
   useEffect(() => {
     console.log('Fetching bonsai with ID: ', id);
+    setBonsai(bonsaiData);
   }, [id]);
 
   return (
     <div>
       <h1>Bonsai Page</h1>
       <p>Displaying details for bonsai with ID: {id}</p>
+      {bonsai && (
+        <div>
+          <p>Species: {bonsai.species}</p>
+          <p>User: {bonsai.user}</p>
+        </div>
+      )}
     </div>
   );
 }
