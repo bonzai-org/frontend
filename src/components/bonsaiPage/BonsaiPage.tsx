a7c7e38beac32df49524ef8edcc56ba3f69f16c6
mrwindupbirdesq <nmaahs11@gmail.com>
2024-08-26 19:04:46 -0700

    Add image gallery, improve display

--- src/components/bonsaiPage/BonsaiPage.tsx
+++ src/components/bonsaiPage/BonsaiPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { bonsaiData } from '../../bonsaiProfDummyData';
+import styles from './BonsaiPage.module.css';
 
 interface BonsaiChapter {
   photoUrls: string[];
@@ -25,20 +26,89 @@ interface Bonsai {
 export default function BonsaiPage() {
   const { id } = useParams<{ id: string }>();
   const [bonsai, setBonsai] = useState<Bonsai | null>(null);
+  const [currentChapterIndex, setCurrentChapterIndex] = useState(0);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   useEffect(() => {
     console.log('Fetching bonsai with ID: ', id);
     setBonsai(bonsaiData);
   }, [id]);
 
+  const handleNextPhoto = () => {
+    if (bonsai && !isProcessing) {
+      setIsProcessing(true);
+      setCurrentChapterIndex(
+        (prevIndex) => (prevIndex + 1) % bonsai.bonsaiChapters.length
+      );
+    }
+  };
+
+  const handlePrevPhoto = () => {
+    if (bonsai && !isProcessing) {
+      setIsProcessing(true);
+      setCurrentChapterIndex(
+        (prevIndex) =>
+          (prevIndex - 1 + bonsai.bonsaiChapters.length) %
+          bonsai.bonsaiChapters.length
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (bonsai) {
+      setIsProcessing(false);
+    }
+  }, [currentChapterIndex, bonsai]);
+
   return (
     <div>
-      <h1>Bonsai Page</h1>
-      <p>Displaying details for bonsai with ID: {id}</p>
       {bonsai && (
         <div>
-          <p>Species: {bonsai.species}</p>
-          <p>User: {bonsai.user}</p>
+          <p>Displaying details for bonsai with ID: {id}</p>
+          <div className={styles.bonsaiData}>
+            <p>Species: {bonsai.species}</p>
+            <p>User: {bonsai.user}</p>
+            {bonsai.geoLocation && <p>Geo Location: {bonsai.geoLocation}</p>}
+            {bonsai.style && <p>Style: {bonsai.style}</p>}
+            {bonsai.height && <p>Height: {bonsai.height}</p>}
+            {bonsai.width && <p>Width: {bonsai.width}</p>}
+            {bonsai.nebari && <p>Nebari: {bonsai.nebari}</p>}
+            <p>Hardiness Zone: {bonsai.hardinessZone}</p>
+          </div>
+          {bonsai.bonsaiChapters.length > 0 && (
+            <div className={styles.chapterContainer}>
+              <h2> Chapter {`${currentChapterIndex + 1}`}</h2>
+              <div className={styles.chapterInfo}>
+                <p className={styles.chapterDate}>
+                  {bonsai.bonsaiChapters[
+                    currentChapterIndex
+                  ].date.toDateString()}
+                </p>
+                <p className={styles.chapterCaption}>
+                  {bonsai.bonsaiChapters[currentChapterIndex].caption}
+                </p>
+              </div>
+
+              <div className={styles.imageGallery}>
+                <button onClick={handlePrevPhoto} disabled={isProcessing}>
+                  {'<'}
+                </button>
+                <div className={styles.imageFrame}>
+                  <img
+                    src={
+                      bonsai.bonsaiChapters[currentChapterIndex].photoUrls[0]
+                    }
+                    alt={`Bonsai Chapter ${currentChapterIndex + 1} Photo`}
+                    className={styles.chapterImage}
+                    onLoad={() => setIsProcessing(false)}
+                  />
+                </div>
+                <button onClick={handleNextPhoto} disabled={isProcessing}>
+                  {'>'}
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
