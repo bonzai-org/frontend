974b5913f4000b3718ac8ecdf9af1644315a5d7c
mrwindupbirdesq <nmaahs11@gmail.com>
2024-08-26 18:05:21 -0700

    Make multiple chapters

--- src/bonsaiProfDummyData.ts
+++ src/bonsaiProfDummyData.ts
@@ -1,4 +1,4 @@
-const bonsaiChapterData = {
+const bonsaiChapterData1 = {
   photoUrls: [
     'https://res.cloudinary.com/dscsiijis/image/upload/v1721414755/IMG_3701_bkure4.jpg'
   ],
@@ -8,10 +8,19 @@ const bonsaiChapterData = {
     "1 year mark. Bonsai collected from Burton, WA forest, where it was growing from a fallen tree, it's base lying perpendicular to the ground"
 };
 
+const bonsaiChapterData2 = {
+  photoUrls: [
+    'https://res.cloudinary.com/dscsiijis/image/upload/v1712589554/wildRoots/sunflower_atfd07.png'
+  ],
+  bonsaiId: '1',
+  date: new Date(),
+  caption: 'Woah it became a sunflower'
+};
+
 const bonsaiData = {
   id: '1',
   user: 'Pablo',
-  bonsaiChapters: [bonsaiChapterData, bonsaiChapterData, bonsaiChapterData],
+  bonsaiChapters: [bonsaiChapterData1, bonsaiChapterData2],
   geoLocation: 'San Francisco, CA',
   hardinessZone: '9A',
   height: '6in',
@@ -23,7 +32,7 @@ const bonsaiData = {
 
 const bonsaiCardData = {
   id: '1',
-  photoUrl: bonsaiChapterData.photoUrls[0],
+  photoUrl: bonsaiChapterData1.photoUrls[0],
   species: bonsaiData.species,
   user: bonsaiData.user,
   location:
@@ -33,4 +42,4 @@ const bonsaiCardData = {
   style: bonsaiData.style
 };
 
-export { bonsaiData, bonsaiChapterData, bonsaiCardData };
+export { bonsaiData, bonsaiChapterData1, bonsaiChapterData2, bonsaiCardData };
