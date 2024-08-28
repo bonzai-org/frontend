3492432194cb65d32c0bd36e3c0965ae4b450cb0
mrwindupbirdesq <nmaahs11@gmail.com>
2024-08-28 10:25:21 -0700

    Use same interface/ data model as bonsai page

--- src/components/bonsaiCard/BonsaiCard.tsx
+++ src/components/bonsaiCard/BonsaiCard.tsx
@@ -2,48 +2,40 @@ import { bonsaiCardData } from '../../bonsaiProfDummyData';
 import { useNavigate } from 'react-router-dom';
 import UserIcon from '../userIcon/UserIcon';
 import styles from './BonsaiCard.module.css';
+import { Bonsai } from '../../interfaces';
 
 // TODO:
 // Refactor to display leading photo
 // from each chapter in a gallery
 
-interface BonsaiCardData {
-  id: string;
-  photoUrl: string;
-  species: string;
-  user: string;
-  location: string;
-  style: string;
-}
-
-function BonsaiCard({ cardData }: { cardData: BonsaiCardData }) {
+function BonsaiCard({ bonsai }: { bonsai: Bonsai }) {
   const navigate = useNavigate();
 
   const handleCardClick = () => {
-    navigate(`/bonsai/${cardData.id}`);
+    navigate(`/bonsai/${bonsai.id}`);
   };
 
   return (
     <div className={styles.cardContainer} onClick={handleCardClick}>
       <div className={styles.bonsaiInfoContainer}>
-        <UserIcon user={{ username: bonsaiCardData.user }} />
+        <UserIcon user={{ username: bonsai.user }} />
         <div className={styles.bonsaiInfoCell}>
           <p>Species: </p>
-          <p>{bonsaiCardData.species}</p>
+          <p>{bonsai.species}</p>
         </div>
 
         <div className={styles.bonsaiInfoCell}>
           <p>Style: </p>
-          <p>{bonsaiCardData.style}</p>
+          <p>{bonsai.style}</p>
         </div>
 
         <div className={styles.bonsaiInfoCell}>
           <p>Location: </p>
-          <p>{bonsaiCardData.location}</p>
+          <p>{bonsai.geoLocation}</p>
         </div>
       </div>
       <div className={styles.imageFrame}>
-        <img className={styles.image} src={bonsaiCardData.photoUrl} alt="" />
+        <img className={styles.image} src={} alt="" />
       </div>
     </div>
   );
