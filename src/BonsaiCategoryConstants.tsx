56d0a36fd5dbdc91d18e61fc3e238359f31320b5
mrwindupbirdesq <nmaahs11@gmail.com>
2024-08-29 10:23:45 -0700

    List other option last

--- src/BonsaiCategoryConstants.tsx
+++ src/BonsaiCategoryConstants.tsx
@@ -4,5 +4,5 @@ export const HARDINESSZONES = ['0a', '0b', '1a', '1b', '2a', '2b', '3a', '3b',
 
 export const BONSAISTYLES = ['Broom style (Hokidachi)', 'Cascade (Kengai)', 'Double trunk (Sokan)',
 'Formal upright (Chokkan)', 'Forest (Yose-ue)', 'Growing in a rock (Ishisuki)', 'Growing on a rock (Seki-Joju)',
-'Informal upright (Moyogi)', 'Literati (Bunjin-gi)', 'Multi-trunk (Ikadabuki)', 'Other', 'Raft (Ikadabuki)',
-'Semi-cascade (Han-kengai)', 'Shari (Sharimiki)', 'Slanting (Shakan)', 'Windswept (Fukinagashi)'];
+'Informal upright (Moyogi)', 'Literati (Bunjin-gi)', 'Multi-trunk (Ikadabuki)',  'Raft (Ikadabuki)',
+'Semi-cascade (Han-kengai)', 'Shari (Sharimiki)', 'Slanting (Shakan)', 'Windswept (Fukinagashi)', 'Other'];
