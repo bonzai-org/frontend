13961eeb7f4c7b95fc2af887ab1a335997ee7666
mrwindupbirdesq <nmaahs11@gmail.com>
2024-09-11 16:51:46 -0700

    Add optional fields const, run prettier

--- src/BonsaiCategoryConstants.tsx
+++ src/BonsaiCategoryConstants.tsx
@@ -1,8 +1,51 @@
-export const HARDINESSZONES = ['0a', '0b', '1a', '1b', '2a', '2b', '3a', '3b', 
-'4a', '4b', '5a', '5b', '6a', '6b', '7a', '7b', '8a', '8b', '9a', '9b',
-'10a', '10b', '11a', '11b', '12a', '12b', '13a', '13b'];
+export const HARDINESSZONES = [
+  '0a',
+  '0b',
+  '1a',
+  '1b',
+  '2a',
+  '2b',
+  '3a',
+  '3b',
+  '4a',
+  '4b',
+  '5a',
+  '5b',
+  '6a',
+  '6b',
+  '7a',
+  '7b',
+  '8a',
+  '8b',
+  '9a',
+  '9b',
+  '10a',
+  '10b',
+  '11a',
+  '11b',
+  '12a',
+  '12b',
+  '13a',
+  '13b'
+];
 
-export const BONSAISTYLES = ['Broom style (Hokidachi)', 'Cascade (Kengai)', 'Double trunk (Sokan)',
-'Formal upright (Chokkan)', 'Forest (Yose-ue)', 'Growing in a rock (Ishisuki)', 'Growing on a rock (Seki-Joju)',
-'Informal upright (Moyogi)', 'Literati (Bunjin-gi)', 'Multi-trunk (Ikadabuki)',  'Raft (Ikadabuki)',
-'Semi-cascade (Han-kengai)', 'Shari (Sharimiki)', 'Slanting (Shakan)', 'Windswept (Fukinagashi)', 'Other'];
+export const BONSAISTYLES = [
+  'Broom style (Hokidachi)',
+  'Cascade (Kengai)',
+  'Double trunk (Sokan)',
+  'Formal upright (Chokkan)',
+  'Forest (Yose-ue)',
+  'Growing in a rock (Ishisuki)',
+  'Growing on a rock (Seki-Joju)',
+  'Informal upright (Moyogi)',
+  'Literati (Bunjin-gi)',
+  'Multi-trunk (Ikadabuki)',
+  'Raft (Ikadabuki)',
+  'Semi-cascade (Han-kengai)',
+  'Shari (Sharimiki)',
+  'Slanting (Shakan)',
+  'Windswept (Fukinagashi)',
+  'Other'
+];
+
+export const OPTIONALBONSAIFIELDS = ['style', 'height', 'width', 'nebari'];
