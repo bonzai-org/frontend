f23371d8c8f13f0be566b136c5efd022c65683e0
mrwindupbirdesq <nmaahs11@gmail.com>
2024-09-16 13:38:24 -0700

    Bonsai interface same as get

--- src/handleFetchRes.ts
+++
@@ -1,9 +0,0 @@
-export async function handleAuthSuccess(response: Response, setErrorMsg: (message: string | null) => void,
-  redir: () => void, setAuth: (username: string, profilePhoto: string) => void) {
-  {
-    setErrorMsg(null);
-    const data = await response.json();
-    setAuth(data.username, data.profilePhoto);
-    redir();
-  }
-}
--- src/interfaces/bonsai.ts
+++ src/interfaces/bonsai.ts
@@ -1,19 +1,21 @@
-export interface Bonsai {
-    id: string;
-    username: string;
-    species: string;
-    geoLocation: string;
-    style?: string;
-    height?: string;
-    width?: string;
-    nebari?: string;
-    hardinessZone: string;
-    bonsaiChapters: BonsaiChapter[];
-  }
+import { UserPartial } from './user';
 
-  export interface BonsaiChapter {
-    photoUrls: string[];
-    date: Date;
-    caption: string;
-    bonsaiId: string;
-  }
+export interface Bonsai {
+  geoLocation: string;
+  hardinessZone: string;
+  species: string;
+  height?: string;
+  width?: string;
+  nebari?: string;
+  style?: string;
+  bonsaiChapters: BonsaiChapter[];
+  user: UserPartial;
+  id: string;
+}
+
+export interface BonsaiChapter {
+  photoUrls: string[];
+  date: Date;
+  caption: string;
+  bonsaiId: string;
+}
